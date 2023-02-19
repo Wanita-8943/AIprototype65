621c23b9d5a0e325980b44cfe752ba80c9bb0b61
root <root@LovelyJeno.localdomain>
2023-02-20 00:07:24 +0700

    HW1

--- python_script101.py
+++ python_script101.py
@@ -10,15 +10,6 @@ def parse_input():
     args = parser.parse_args()
     return args
 
-#HW สั่งรัน python_script101.py 50 รอบ โดย x = 1,3,5,7,... 
-# แล้วให้เอา output ของ xt มารวมกัน แล้ว print ออกมา
-
-x = range(1, 100, 2)
-print('x = ',list(x))
-print('count:', (len(x)))
-
-xt = sum(x) 
-print('sum = ', xt) 
  
 # # def print_other():
 # #     print('something else')
@@ -26,13 +17,13 @@ print('sum = ', xt)
 # # def print_ones():
 # #     print('1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1')
 
-# if __name__ == "__main__":
-#     x = parse_input()
-# #     print_ones()
-# #     print_other()
-# #     print_ones()
+if __name__ == "__main__":
+    x = parse_input()
+#     print_ones()
+#     print_other()
+#     print_ones()
 
-#     print(f'YVAL = {x.yval}')
-#     print(f'xt = {(x.x)*2}')
+    # print(f'YVAL = {x.yval}')
+    print(f'     xt = {(x.x)*2}')
     # print(f'm = {x.m}')
     # print(f'n = {x.n}')
--- python_subprocess.py
+++ python_subprocess.py
@@ -6,28 +6,23 @@ if __name__ == "__main__":
     # subprocess.run(["python", "python_script101.py","--x","8"])
    
 
-    process = subprocess.Popen(["python", "python_script101.py","--x","8"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = process.communicate()
-    print(out.decode('utf-8'))
+    # process = subprocess.Popen(["python", "python_script101.py","--x","8"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # out, err = process.communicate()
+    # print(out.decode('utf-8'))
 
 #HW สั่งรัน python_script101.py 50 รอบ โดย x = 1,3,5,7,... 
 # แล้วให้เอา output ของ xt มารวมกัน แล้ว print ออกมา
     
-    # print('HW1')
-    # x = range(1, 100, 2)
-    # print('x = ',list(x))
-    # process = subprocess.Popen(["python", "python_script101.py","--x", f"{x}"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # out, err = process.communicate()
-    # print(out.decode('utf-8'))
-
-    # process = subprocess.Popen(["python", "python_script_101.py", "--x", f"{x}"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # for i in range(1, 100, 2):
-    #     process = subprocess.Popen(["python", "python_script_101.py", "--x", f"{i}"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #     out, err = process.communicate()
-    #     x = out.decode('utf-8')
-    #     # print(f"(Input x: {i}")
-    #     # print(x)
-    #     print(out.decode('utf-8'))
-    #     xt = list(i)
-    #     print(xt)
-    #     print('sum = ', xt)
+    sum = 0 
+    round = 0
+    for i in range(1, 100, 2):
+            round +=1
+            process1 = subprocess.Popen(["python", "python_script101.py", "--x",f'{i}'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process1.communicate()
+            print(f"({round}) x= {i}")
+            print(out.decode('utf-8'))
+            sum += (i)
+            
+    print(f'   sum of x = {sum}')
+    print(f'   sum of xt = {(sum)*2}')
+   
